e748f4c5aa4bddc803c6fb652057f7341c598a61
r-drg <rodrigohferreira210@gmail.com>
2022-06-27 18:37:27 -0300

    chore: remove sensitive data

--- hardhat.config.js
+++ hardhat.config.js
@@ -11,6 +11,8 @@ const ALCHEMY_API_URL = process.env.ALCHEMY_API_URL ?? "";
 
 const WALLET_PRIVATE_KEY = process.env.WALLET_PRIVATE_KEY ?? "";
 
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY ?? "";
+
 module.exports = {
   solidity: "0.8.15",
   networks: {
@@ -20,5 +22,6 @@ module.exports = {
     },
   },
   etherscan: {
+    apiKey: ETHERSCAN_API_KEY,
   },
 };
