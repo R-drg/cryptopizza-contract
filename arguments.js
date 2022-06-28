03ed3291a4b71d3b1a7536843e265a53321be44a
Rodrigo Ferreira <rodrigohferreira210@gmail.com>
2022-06-28 15:55:40 -0300

    fix: changed argument orders correctly

--- arguments.js
+++ arguments.js
@@ -5,11 +5,11 @@ module.exports = [
     "Tomato sauce, portuguese ham, eggs, mozzarella cheese",
     "Tomato sauce, champignon, mozzarella cheese",
   ],
+  ["20000000000000000", "25000000000000000", "30000000000000000"],
   [
     "https://i.ibb.co/xhFLTvk/pepperoni.png",
     "https://i.ibb.co/b1mkz2Y/portuguese.png",
     "https://i.ibb.co/qsGnXDS/champignon.png",
   ],
-  ["20000000000000000", "25000000000000000", "30000000000000000"],
   "https://gateway.pinata.cloud/ipfs/Qmckm5C1k47HH5yYuYAcmVFSnxzUcBeEXwx1Zhhdbjmvm9",
 ];
